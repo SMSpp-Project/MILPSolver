7d4398bf879b3957a546fde8727fee7f7861a21e
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2019-06-21 16:52:10 +0200

    Major fixes in Doxygen comments

--- include/CPXMILPSolver.h
+++ include/CPXMILPSolver.h
@@ -2,9 +2,10 @@
 /*--------------------------- File CPXMILPSolver.h -------------------------*/
 /*--------------------------------------------------------------------------*/
 /** @file
- * Header file for the CPLEX MILP Solver class, that defines a General Purpose
- * Solver that is able to tackle a MILP Problem expressed by a Block via the
- * use of IMB CLPEX.
+ * Header file for the CPXMILPSolver class.
+ *
+ * CPXMILPSolver implements a general purpose solver that is able to tackle a
+ * MILP problem expressed by a Block using IBM CLPEX.
  *
  * \version 0.90
  *
@@ -13,7 +14,7 @@
  * \author Antonio Frangioni \n
  *         Operations Research Group \n
  *         Dipartimento di Informatica \n
- *         Universita' di Pisa \n
+ *         Università di Pisa \n
  *
  * \author Niccolò Iardella \n
  *         Operations Research Group \n
@@ -23,9 +24,9 @@
  * \author Kostas Tavlaridis-Gyparakis \n
  *         Operations Research Group \n
  *         Dipartimento di Informatica \n
- *         Universita' di Pisa \n
+ *         Università di Pisa \n
  *
- * Copyright &copy by Antonio Frangioni, Kostas Tavlaridis-Gyparakis, Niccolò Iardella
+ * \copyright &copy; Antonio Frangioni, Kostas Tavlaridis-Gyparakis, Niccolò Iardella
  */
 /*--------------------------------------------------------------------------*/
 /*----------------------------- DEFINITIONS --------------------------------*/
@@ -64,22 +65,20 @@ namespace SMSpp_di_unipi_it {
 /*--------------------------- GENERAL NOTES --------------------------------*/
 /*--------------------------------------------------------------------------*/
 
+/// Class for solving MILP problems via CPLEX.
 /**
- * Derived class for solving MILP Problems via CPLEX
+ * The CPXMILPSolver class derives from MILPSolver and extends the
+ * base class to solve MILP problems using CPLEX.
  *
- * The CPXMILPSolver class derives from MILPSolver [see MILPSolver.h] and
- * extends the base class to solve MILP Problems using CPLEX.
+ * The CPXMILPSolver can be registered to any kind of Block (9assuming that
+ * it contains a MILP formulation) and it uses the base class functionalities
+ * to build a matricial representation of a MILP problem, then it solves it
+ * using CPLEX through its Callable Library. Moreover, it implements the
+ * interface that MILPSolver provides for processing modifications.
  *
- * The CPXMILPSolver can be registered to any kind of Block, assuming that
- * it contains a MILP formulation, and it uses the base class functionalities
- * to build a matricial representation of a MILP, then it solves the problem
- * using CPLEX through its Callable Library.
- * Moreover, it implements the interface that MILPSolver provides for
- * processing modifications.
- *
- * The main logic is in compute(). This method copies the LP vectors of the
- * class into a CPLEX environment, it processes the modifications and then
- * it solves the problem.
+ * The main logic is in compute(). This method copies the vectors that decribe
+ * the LP problem into a CPLEX environment, it processes the modifications and
+ * then solves the problem.
  * get_var_solution() retrieves the values of the variables from CPLEX, saves
  * them into the Block variables and evaluates the objective function.
  *
@@ -101,12 +100,8 @@ class CPXMILPSolver : public MILPSolver {
 /*--------------------------------------------------------------------------*/
 /*---------------------------- PUBLIC TYPES --------------------------------*/
 /*--------------------------------------------------------------------------*/
-/**
- * @name Public Types
- * @{
- */
 
- /**
+ /** Types of string parameters.
   * Public enum describing the different types of algorithmic parameters
   * of "string" type that the CPXMILPSolver has.
   */
@@ -116,7 +111,7 @@ class CPXMILPSolver : public MILPSolver {
   strLastAlgParCPXS
  };
 
-/*@}------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /*--------------------- CONSTRUCTOR AND DESTRUCTOR -------------------------*/
 /*--------------------------------------------------------------------------*/
 
@@ -128,13 +123,14 @@ class CPXMILPSolver : public MILPSolver {
  CPXMILPSolver();
 
  ~CPXMILPSolver() override;
+ /// @}
 
-/*@}------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /*--------------------- DERIVED METHODS OF BASE CLASS ----------------------*/
 /*--------------------------------------------------------------------------*/
 
 /**
- * @name Public Methods derived of the Base Class
+ * @name Public Methods derived from base classes
  * @{
  */
 
@@ -155,13 +151,13 @@ class CPXMILPSolver : public MILPSolver {
  void set_par(idx_type par, double value) override;
 
  void set_par(idx_type par, const std::string & value) override;
+ /// @}
 
-
-/*@}------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /*------------------- METHODS FOR HANDLING THE PARAMETERS ------------------*/
 /*--------------------------------------------------------------------------*/
 /**
- * @name Handling the parameters of the CPXMILPSolver
+ * @name Methods for handling parameters
  * @{
  */
 
@@ -172,15 +168,11 @@ class CPXMILPSolver : public MILPSolver {
  idx_type str_par_str2idx(const std::string& name) const override;
 
  const std::string& dbl_par_idx2str(idx_type idx) const override;
+ /// @}
 
-/*@}------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /*-------------------- PROTECTED FIELDS OF THE CLASS -----------------------*/
 /*--------------------------------------------------------------------------*/
-
-/**
- * @name Protected Fields of the MILPSolver
- * @{
- */
 
  protected:
 
@@ -190,41 +182,48 @@ class CPXMILPSolver : public MILPSolver {
  std::string prob_name;   /// CPLEX problem name
  std::string output_file; /// Output file for CPXwriteprob
 
- /**
-  * It removes the problem from the CPLEX environment
+ /** @name Clear and load the problem
+  *
+  * The following two methods include the main logic of the class.
+  * They are called in the set_Block() method to build the problem when the
+  * Solver is [re]registered to a Block, but also when a NBModification is
+  * processed.
+  * Note: These two override the base class methods but call them after running
+  * the specific operations for this class.
+  *
+  * @{
   */
- void clear_problem() override;
 
- /**
-  * It loads the problem into CPLEX environment
-  */
- void load_problem() override;
+ void clear_problem() override; ///< It clears all the LP vectors
 
-/*@}------------------------------------------------------------------------*/
+ void load_problem() override;  ///< It loads all the LP vectors
+ /// @}
+
+/*--------------------------------------------------------------------------*/
 /*-------------------- METHODS FOR MODIFYING THE PROBLEM -------------------*/
 /*--------------------------------------------------------------------------*/
 
 /**
- * @name Methods for modifying the constructed CPLEX Problem
+ * @name Methods for modifying the constructed CPLEX problem
  * @{
  */
 
- /// It handles a Variable Modification
+ /// It handles a variable modification
  void var_modification(VariableMod* mod) override;
 
- /// It handles an Objective Modification
+ /// It handles an objective modification
  void of_modification(ObjectiveMod* mod) override;
 
- /// It handles a RowConstraint Modification
+ /// It handles a constraint modification
  void const_modification(ConstraintMod* mod) override;
 
- /// It handles a OneVarConstraint Modification
+ /// It handles a bound modification
  void bound_modification(OneVarConstraintMod* mod) override;
 
- /// It handles a Function Modification
+ /// It handles a function modification
  void function_modification(FunctionMod* mod) override;
 
- /// It handles a dynamic Modification
+ /// It handles a dynamic modification
  void dynamic_modification(BlockModAD* mod) override;
 
  /// It adds a single new dynamic constraint
@@ -238,16 +237,12 @@ class CPXMILPSolver : public MILPSolver {
 
  /// It removes a single dynamic variable
  void remove_dynamic_variable(ColVariable* p_var) override;
+ /// @}
 
-/*@}------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /*--------------------- PRIVATE FIELDS OF THE CLASS ------------------------*/
 /*--------------------------------------------------------------------------*/
  private:
-
-/**
- * @name Private methods
- * @{
- */
 
  /**
   * It sets the value of a Colvariable taking it from tmpx[i], then increments i.
@@ -259,7 +254,7 @@ class CPXMILPSolver : public MILPSolver {
   * @param i The position in the array
   */
  void set_var_value(ColVariable& lvar, double* tmpx, int& i);
-/*@}------------------------------------------------------------------------*/
+
  SMSpp_insert_in_factory_h;
 };
 
