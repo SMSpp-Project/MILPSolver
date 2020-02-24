e5dcc0ab6af6206b3f5b792f912448cf69a037ab
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-02-24 09:18:50 +0100

    Add Conan recipe

---
+++ conanfile.py
@@ -0,0 +1,56 @@
+from conans import ConanFile, CMake, tools
+
+
+class SmsppConan(ConanFile):
+    name = "milpsolver"
+    version = "0.1.1"
+    description = "A MILP Solver for SMS++"
+    topics = ("conan", "smspp", "milpsolver")
+    url = "https://gitlab.com/smspp/milpsolver"
+    homepage = "https://gitlab.com/smspp/mcfblock"
+    license = "GPL-3.0-only"
+    generators = "cmake"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    requires = (
+        "smspp/0.1.1@smspp/testing",
+        "CPLEX/12.10"
+    )
+
+    exports_sources = [
+        "CMakeLists.txt",
+        "src/*",
+        "include/*",
+        "cmake/*"
+    ]
+
+    def source(self):
+        tools.replace_in_file(
+            "CMakeLists.txt",
+            '''project(MILPSolver VERSION 0.1.1 LANGUAGES CXX)''',
+            '''project(MILPSolver VERSION 0.1.1 LANGUAGES CXX)\n''' +
+            '''include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n''' +
+            '''conan_basic_setup()'''
+        )
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_TESTING"] = False
+        cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses")
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.includedirs = ["include", "include/SMS++"]
+        self.cpp_info.libs = ["MILPSolver"]
