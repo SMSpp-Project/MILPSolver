f0470bedbfdd64a094b8161d2c7b16504d092614
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-09-16 17:06:49 +0200

    Bump version number and update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,9 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.0] - 2020-09-16
+
 ### Added
 
-- Support for concurrency
+- Support for concurrency.
 
 ## [0.2.0] - 2020-03-06
 
@@ -33,7 +35,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - First test release.
 
-[Unreleased]: https://gitlab.com/smspp/milpsolver/-/compare/0.2.0...develop
+[Unreleased]: https://gitlab.com/smspp/milpsolver/-/compare/0.3.0...develop
+[0.3.0]: https://gitlab.com/smspp/milpsolver/-/compare/0.2.0...0.3.0
 [0.2.0]: https://gitlab.com/smspp/milpsolver/-/compare/0.1.0...0.2.0
 [0.1.1]: https://gitlab.com/smspp/milpsolver/-/compare/0.1.0...0.1.1
 [0.1.0]: https://gitlab.com/smspp/milpsolver/-/tags/0.1.0
--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class SmsppConan(ConanFile):
     name = "milpsolver"
-    version = "0.2.0"
+    version = "0.3.0"
     description = "A MILP Solver for SMS++"
     topics = ("conan", "smspp", "milpsolver")
     url = "https://gitlab.com/smspp/milpsolver"
@@ -16,7 +16,7 @@ class SmsppConan(ConanFile):
     default_options = {"shared": False, "fPIC": True}
 
     requires = (
-        "smspp/0.2.0@smspp/testing",
+        "smspp/0.3.0@smspp/testing",
         "CPLEX/12.10"
     )
 
@@ -30,8 +30,8 @@ class SmsppConan(ConanFile):
     def source(self):
         tools.replace_in_file(
             "CMakeLists.txt",
-            '''project(MILPSolver VERSION 0.2.0 LANGUAGES CXX)''',
-            '''project(MILPSolver VERSION 0.2.0 LANGUAGES CXX)\n''' +
+            '''project(MILPSolver VERSION 0.3.0 LANGUAGES CXX)''',
+            '''project(MILPSolver VERSION 0.3.0 LANGUAGES CXX)\n''' +
             '''include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n''' +
             '''conan_basic_setup()'''
         )
