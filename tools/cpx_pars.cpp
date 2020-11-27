fe727b8569e1596b87d82515b43fc988ec13e5d4
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-11-27 13:01:15 +0100

    Change the tools

--- tools/cpx_pars.cpp
+++ tools/cpx_pars.cpp
@@ -6,8 +6,8 @@
  * the support for plain CPLEX parameters into CPXMILPSolver.
  *
  * The tool generates two files, CPX<CPX_VERSION>_defs.h and
- * CPX<CPX_VERSION>_maps.h. These are automatically placed in the include
- * directory of the MILPSolver project (assumed to be ../include).
+ * CPX<CPX_VERSION>_maps.h in the specified path.
+ * The path should be the include directory of the MILPSolver source tree.
  *
  * \author Niccolo' Iardella \n
  *         Operations Research Group \n
@@ -23,15 +23,100 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <ilcplex/cplex.h>
 #include <map>
+#include <filesystem>
+#include <getopt.h>
+
+#include <ilcplex/cplex.h>
+
+/*--------------------------------------------------------------------------*/
+
+bool verbose = false;         ///< If the tool should be verbose
+std::filesystem::path path{}; ///< Path for output files
+std::string exe{};            ///< Name of the executable file
+std::string docopt_desc{};    ///< Tool description
+
+/*--------------------------------------------------------------------------*/
+
+/// Gets the name of the executable from its full path
+std::string get_filename( const std::string & fullpath ) {
+ std::size_t found = fullpath.find_last_of( "/\\" );
+ return fullpath.substr( found + 1 );
+}
+
+/*--------------------------------------------------------------------------*/
+
+/// Prints the tool description and usage
+void docopt() {
+ // http://docopt.org
+ std::cout << docopt_desc << std::endl;
+ std::cout << "Usage:\n"
+           << "  " << exe << " [-v] <path>\n"
+           << "  " << exe << " -h | --help\n"
+           << std::endl
+           << "Options:\n"
+           << "  -v, --verbose  Make the tool verbose.\n"
+           << "  -h, --help     Print this help.\n";
+}
+
+/*--------------------------------------------------------------------------*/
+
+/// Processes the command line arguments
+void process_args( int argc, char ** argv ) {
+
+ const char * const short_opts = "vh";
+ const option long_opts[] = {
+  { "verbose", no_argument, nullptr, 'v' },
+  { "help",    no_argument, nullptr, 'h' },
+  { nullptr,   no_argument, nullptr, 0 }
+ };
+
+ // Options
+ while( true ) {
+  const auto opt = getopt_long( argc, argv, short_opts, long_opts, nullptr );
+
+  if( -1 == opt ) {
+   break;
+  }
+  switch( opt ) {
+   case 'v':
+    verbose = true;
+    break;
+   case 'h':
+    docopt();
+    exit( 0 );
+   case '?':
+   default:
+    std::cout << "Try " << exe << "' --help' for more information.\n";
+    exit( 1 );
+  }
+ }
+
+ // Last argument
+ if( optind < argc ) {
+  path = std::string( argv[ optind ] );
+ }
+}
+
+/*--------------------------------------------------------------------------*/
 
 int main( int argc, char ** argv ) {
 
- std::string defs_file_name(
-  "../include/CPX" + std::to_string( CPX_VERSION ) + "_defs.h" );
- std::string maps_file_name(
-  "../include/CPX" + std::to_string( CPX_VERSION ) + "_maps.h" );
+ // Manage options and help
+ path = std::filesystem::current_path();
+ docopt_desc = "CPLEX parameter map generator.\n";
+ exe = get_filename( argv[ 0 ] );
+ process_args( argc, argv );
+
+ if (!std::filesystem::exists(path)) {
+  std::filesystem::create_directory(path);
+ }
+
+ std::string defs_filename = "CPX" + std::to_string( CPX_VERSION ) + "_defs.h";
+ std::string maps_filename = "CPX" + std::to_string( CPX_VERSION ) + "_maps.h";
+
+ auto defs_path = path / defs_filename;
+ auto maps_path = path / maps_filename;
 
  std::ofstream defs_file;
  std::ofstream maps_file;
@@ -50,7 +135,9 @@ int main( int argc, char ** argv ) {
  int total = 0;
 
  env = CPXopenCPLEX( &status );
- std::cout << "CPX_VERSION is " << CPX_VERSION << std::endl;
+ if( verbose ) {
+  std::cout << "CPX_VERSION is " << CPX_VERSION << std::endl;
+ }
 
  for( int i = CPX_PARAM_ALL_MIN; i <= CPX_PARAM_ALL_MAX; ++i ) {
 
@@ -107,7 +194,7 @@ int main( int argc, char ** argv ) {
  }
 
  // Generate defs file
- defs_file.open( defs_file_name );
+ defs_file.open( defs_path );
 
  defs_file << "/* FILE GENERATED AUTOMATICALLY, DO NOT EDIT */" << std::endl
            << std::endl
@@ -123,10 +210,10 @@ int main( int argc, char ** argv ) {
            << std::endl;
 
  defs_file.close();
- std::cout << "Defs file written on " << defs_file_name << std::endl;
+ std::cout << "Defs file written on " << defs_path << std::endl;
 
  // Generate maps file
- maps_file.open( maps_file_name );
+ maps_file.open( maps_path );
  maps_file << "/* FILE GENERATED AUTOMATICALLY, DO NOT EDIT */" << std::endl
            << std::endl
            << "#include <ilcplex/cplex.h>" << std::endl
@@ -205,7 +292,7 @@ int main( int argc, char ** argv ) {
   << " }" << std::endl
   << "};" << std::endl;
  maps_file.close();
- std::cout << "Maps file written on " << maps_file_name << std::endl;
+ std::cout << "Maps file written on " << maps_path << std::endl;
 
  return 0;
 }
