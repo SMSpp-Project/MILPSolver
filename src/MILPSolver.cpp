fe13e4103d77be209e7c6fd1b838b0b402127aa3
Rafael Lobato <rafael.lobato@gmail.com>
2023-05-17 01:29:04 -0300

    Fix copyright information

--- src/MILPSolver.cpp
+++ src/MILPSolver.cpp
@@ -12,7 +12,7 @@
  *         Dipartimento di Informatica \n
  *         Universita' di Pisa \n
  *
- * Copyright &copy; by Antonio Frangioni, Niccolo' Iardella
+ * \copyright Copyright &copy; by Antonio Frangioni, Niccolo' Iardella
  */
 /*--------------------------------------------------------------------------*/
 /*---------------------------- IMPLEMENTATION ------------------------------*/
