25579956d3b53b5dd1a2e26234f11ff71012588d
dmeoli <donato.meoli.95@gmail.com>
2025-02-10 18:30:48 +0100

    minor updates

--- cmake/FindGUROBI.cmake
+++ cmake/FindGUROBI.cmake
@@ -25,7 +25,6 @@
 #    This find module is provided because GUROBI does not provide             #
 #    a CMake configuration file on its own.                                   #
 #                                                                             #
-#                              Niccolo' Iardella                              #
 #                                Donato Meoli                                 #
 #                         Dipartimento di Informatica                         #
 #                             Universita' di Pisa                             #
