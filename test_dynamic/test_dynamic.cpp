1b93a0d0cf6872d6974969476df8a95008f1a849
Donato Meoli <donato.meoli.95@gmail.com>
2026-03-09 08:48:46 -0500

    spaces

--- test_dynamic/test_dynamic.cpp
+++ test_dynamic/test_dynamic.cpp
@@ -857,7 +857,6 @@ static bool SolveAll( void )
 /// Custom terminate function to print the exception message
 
 void smspp_terminate( void ) {
-
  std::cerr << "Uncaught exception in executing SMS++:\n";
  try {
   std::rethrow_exception( std::current_exception() );
--- tools/scip_pars.cpp
+++ tools/scip_pars.cpp
@@ -108,7 +108,6 @@ void process_args( int argc , char ** argv )
 /// Custom terminate function to print the exception message
 
 void smspp_terminate( void ) {
-
  std::cerr << "Uncaught exception in executing SMS++:\n";
  try {
   std::rethrow_exception( std::current_exception() );
