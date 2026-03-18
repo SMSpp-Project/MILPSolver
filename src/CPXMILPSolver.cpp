747286ef35690c2ecd0600fd631c5bb79b64a188
enrico_calandrini <calandrini.enrico7@gmail.com>
2026-03-18 17:48:51 +0000

    Fixed index mismatch in CPXMILPSolver when dealing with quadratic constraints

--- src/CPXMILPSolver.cpp
+++ src/CPXMILPSolver.cpp
@@ -366,7 +366,7 @@ void CPXMILPSolver::load_problem( void )
         // Add new auxiliary variable with coeficient 1 in the row
         std::vector< double > v_lb = { -CPX_INFBOUND };
         std::vector< double > v_ub = { CPX_INFBOUND };
-        std::string tmp = "quad_aux_var_" + std::to_string( count_quad );
+        std::string tmp = "quad_aux_var_" + std::to_string( num_qauxvar );
         std::vector< char * > v_name( 1 );
         v_name[ 0 ] = strcpy( new char[ tmp.length() + 1 ] , tmp.c_str() );
 
@@ -375,7 +375,7 @@ void CPXMILPSolver::load_problem( void )
 
         delete[] v_name[ 0 ];
 
-        rmatind.push_back( numcols + count_quad );
+        rmatind.push_back( numcols + num_qauxvar );
         rmatval.push_back( 1 );
 
         // update the CPLEX problem with q x + v <= q_0
@@ -384,10 +384,10 @@ void CPXMILPSolver::load_problem( void )
                   rmatval.data() , nullptr , &name );
 
         // Now we have to create the auxiliary quadratic constraint
-        std::vector< int > lidx = { numcols + count_quad };
+        std::vector< int > lidx = { numcols + num_qauxvar };
         std::vector< double > lcoeff = { 1 };
 
-        std::string tmp_con = "quad_aux_con_" + std::to_string( count_quad );
+        std::string tmp_con = "quad_aux_con_" + std::to_string( num_qauxvar );
 
         CPXaddqconstr( env , lp , 1 , qidx1.size() , 0 ,
           sense_q , lidx.data() , lcoeff.data() , qidx1.data() ,
